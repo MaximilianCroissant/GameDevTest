e7efe638c19e264ee188fd5c6112a398a9b697ab
Amy Smith <axs1534@student.bham.ac.uk>
2020-11-02 11:32:50 +0000

    first commit of everything in Physics Toy

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    //public GameObject deathWall;
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "DeathWall") {
+
+            Destroy(gameObject);
+
+        }
+
+    }
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SpawnBalls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour { 
+
+    public GameObject spawnPosition;
+
+    public GameObject ball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject newBall = Instantiate(ball);
+            newBall.transform.position = spawnPosition.transform.position;
+        }
+    }
+
+}
